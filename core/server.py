212c0973f5e8517f232147ec82f33f21f3dfc038
Ishika-786 <ishusarita1bb@gmail.com>
2024-09-18 14:59:58 +0530

    Update server.py

--- core/server.py
+++ core/server.py
@@ -42,3 +42,22 @@ def handle_error(err):
         ), err.code
 
     raise err
+
+from flask import jsonify
+from marshmallow.exceptions import ValidationError
+from core import app
+from core.apis.assignments import student_assignments_resources, teacher_assignments_resources
+from core.apis.assignments import student_assignments_resources, teacher_assignments_resources,principal_assignments_resources
+from core.libs import helpers
+from core.libs.exceptions import FyleError
+from werkzeug.exceptions import HTTPException
+@@ -10,6 +10,8 @@
+
+app.register_blueprint(student_assignments_resources, url_prefix='/student')
+app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
+app.register_blueprint(principal_assignments_resources, url_prefix='/principal')
+
+
+
+@app.route('/')
+ 4 changes: 4 additions & 0 deletions4
